7d27a216348a4a3c7fe5953b906c200a46abbcc6
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-26 21:23:08 -0300

    Print of the chess board

---
+++ xadrez/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+
+	}
+
+}
