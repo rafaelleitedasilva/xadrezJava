a569170b6cfbb913545ff4055bc9390c9f3f0edd
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-28 13:41:36 -0300

    Moving Pieces

--- xadrez/src/application/UI.java
+++ xadrez/src/application/UI.java
@@ -1,10 +1,17 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-
+	
+//Para que as cores funcionassem no terminal eu tive que mudar a versão
+//do JRE para a versão 1.8
+	
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -24,6 +31,18 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+		String s = sc.nextLine();
+		char column = s.charAt(0);
+		int row = Integer.parseInt(s.substring(1));
+		return new ChessPosition(column, row);
+		}catch(RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
+	
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -49,4 +68,6 @@ public class UI {
         }
         System.out.print(" ");
 	}
+	
+	
 }
--- xadrez/src/boardgame/Board.java
+++ xadrez/src/boardgame/Board.java
@@ -43,6 +43,20 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -57,5 +71,7 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	
 	
 }
